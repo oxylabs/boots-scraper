f8f3b1d67b0fbf71f0b81c1267978be985bdaabd
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:39:15 +0200

    Adding code examples/boots_nodejs.js

---
+++ code examples/boots_nodejs.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+const body = {
+  'source': 'universal_ecommerce',
+  'url': 'https://www.boots.ie/sale?paging.index=0&paging.size=48&sortby=mostrelevant&criteria.category=sale',
+};
+const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+  }
+});
+
+console.log(await response.json());
